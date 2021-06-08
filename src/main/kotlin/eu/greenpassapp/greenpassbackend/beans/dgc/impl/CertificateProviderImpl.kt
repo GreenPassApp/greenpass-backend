d9d721caba4b6837fe545860c6402b3b08b197e6
Jakob Stadlhuber <jakob@stadlhuber.org>
2021-06-08 23:02:33 +0200

    added certificates from files

--- src/main/kotlin/eu/greenpassapp/greenpassbackend/beans/dgc/impl/CertificateProviderImpl.kt
+++ src/main/kotlin/eu/greenpassapp/greenpassbackend/beans/dgc/impl/CertificateProviderImpl.kt
@@ -1,21 +1,37 @@
 package eu.greenpassapp.greenpassbackend.beans.dgc.impl
 
 import com.nimbusds.jose.util.Base64
+import org.bouncycastle.util.io.pem.PemReader
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 import se.digg.dgc.signatures.CertificateProvider
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
+import kotlin.io.path.name
+import kotlin.io.path.nameWithoutExtension
 
 @Service
-class CertificateProviderImpl : CertificateProvider {
-    val certificates = mapOf(
-        "2Rk3X8HntrI=" to
-"""-----BEGIN CERTIFICATE-----
-MIIBvTCCAWOgAwIBAgIKAXk8i88OleLsuTAKBggqhkjOPQQDAjA2MRYwFAYDVQQDDA1BVCBER0MgQ1NDQSAxMQswCQYDVQQGEwJBVDEPMA0GA1UECgwGQk1TR1BLMB4XDTIxMDUwNTEyNDEwNloXDTIzMDUwNTEyNDEwNlowPTERMA8GA1UEAwwIQVQgRFNDIDExCzAJBgNVBAYTAkFUMQ8wDQYDVQQKDAZCTVNHUEsxCjAIBgNVBAUTATEwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASt1Vz1rRuW1HqObUE9MDe7RzIk1gq4XW5GTyHuHTj5cFEn2Rge37+hINfCZZcozpwQKdyaporPUP1TE7UWl0F3o1IwUDAOBgNVHQ8BAf8EBAMCB4AwHQYDVR0OBBYEFO49y1ISb6cvXshLcp8UUp9VoGLQMB8GA1UdIwQYMBaAFP7JKEOflGEvef2iMdtopsetwGGeMAoGCCqGSM49BAMCA0gAMEUCIQDG2opotWG8tJXN84ZZqT6wUBz9KF8D+z9NukYvnUEQ3QIgdBLFSTSiDt0UJaDF6St2bkUQuVHW6fQbONd731/M4nc=
------END CERTIFICATE-----"""
-    )
+class CertificateProviderImpl(
+    @Value("\${cert.path}") private val certPath: String
+) : CertificateProvider {
+    val certificates = mutableMapOf<String, String>()
+
+    init {
+        Files.walk(Paths.get(certPath)).use { paths ->
+            paths
+                .filter(Files::isRegularFile)
+                .forEach {
+                    val file = FileReader("$certPath/${it.name}")
+                    val reader = PemReader(file)
+                    certificates[it.nameWithoutExtension] = reader.readText()
+                }
+        }
+    }
 
     override fun getCertificates(country: String?, kid: ByteArray?): MutableList<X509Certificate> {
         val encodedKid = Base64.encode(kid).toString()
