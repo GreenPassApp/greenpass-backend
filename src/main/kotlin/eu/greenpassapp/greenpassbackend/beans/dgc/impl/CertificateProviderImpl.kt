4a6525cbc4443d2fb7189dd9a19f333177bb0c7d
Jakob Stadlhuber <jakob@stadlhuber.org>
2021-06-22 01:54:40 +0200

    added trustlist validator

--- src/main/kotlin/eu/greenpassapp/greenpassbackend/beans/dgc/impl/CertificateProviderImpl.kt
+++ src/main/kotlin/eu/greenpassapp/greenpassbackend/beans/dgc/impl/CertificateProviderImpl.kt
@@ -1,7 +1,13 @@
 package eu.greenpassapp.greenpassbackend.beans.dgc.impl
 
 import com.beust.klaxon.Klaxon
-import com.nimbusds.jose.util.Base64
+import org.bouncycastle.asn1.ASN1Integer
+import org.bouncycastle.asn1.DERSequence
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
+import org.bouncycastle.openssl.PEMParser
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
+import org.bouncycastle.util.io.pem.PemReader
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -9,39 +15,103 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 import se.digg.dgc.signatures.CertificateProvider
+import java.io.FileReader
+import java.math.BigInteger
 import java.net.URL
+import java.security.PublicKey
+import java.security.Signature
+import java.security.SignatureException
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
 
 @Service
 @Configuration
 @EnableScheduling
-class CertificateProviderImpl() : CertificateProvider {
-    var certificates = mutableMapOf<String, String>()
+class CertificateProviderImpl(
+    @Value("\${cert.path}") private val certPath: String
+) : CertificateProvider {
+    private var certificates = mutableMapOf<String, String>()
+    private var publicKey: PublicKey
 
     init {
+        val file = FileReader("$certPath/certs.pub")
+        val reader = PemReader(file)
+        publicKey = readPemKeys(reader.readText()).first()
         reloadCerts()
     }
 
     @Scheduled(fixedRate = 1000 * 60 * 60 * 12) //every 12h
     private fun reloadCerts() {
         val trustList = URL("https://de.dscg.ubirch.com/trustList/DSC/").readText()
-        var json = trustList.substringAfter("\"certificates\":")
-        json = json.substring(0, json.length - 1)
-        val parsedCerts = Klaxon().parseArray<PublicCert>(json)
+        val json = decodeDscList(trustList)
+
+        val parsedCerts = Klaxon().parse<TrustList>(json)
         certificates = mutableMapOf()
-        parsedCerts?.forEach {
+        parsedCerts?.certificates?.forEach {
             certificates[it.kid] = "-----BEGIN CERTIFICATE-----\n${it.rawData}\n-----END CERTIFICATE-----"
         }
     }
 
     override fun getCertificates(country: String?, kid: ByteArray?): MutableList<X509Certificate> {
-        val encodedKid = Base64.encode(kid).toString()
+        val encodedKid = com.nimbusds.jose.util.Base64.encode(kid).toString()
         val cf = CertificateFactory.getInstance("X.509")
         val cert = certificates[encodedKid] ?: throw ResponseStatusException(
             HttpStatus.NOT_FOUND,
             "Can't find a matching certificate"
         )
         return mutableListOf(cf.generateCertificate(cert.byteInputStream()) as X509Certificate)
+    }
+
+    private fun decodeDscList(data: String): String {
+        val encodedSignature = data.substringBefore("{")
+        val signature = org.bouncycastle.util.encoders.Base64.decode(encodedSignature)
+        val trustedList = data.substring(encodedSignature.length).trim()
+        validateSignature(
+            key = publicKey,
+            data = trustedList.toByteArray(),
+            signature = signature,
+            algorithm = "SHA256withECDSA",
+        )
+        return trustedList
+    }
+
+    private fun validateSignature(key: PublicKey, data: ByteArray, signature: ByteArray, algorithm: String) {
+        val javaSignature = if (algorithm.endsWith("withECDSA")) {
+            val (r, s) = signature.splitHalves()
+            DERSequence(
+                arrayOf(
+                    ASN1Integer(BigInteger(1, r)),
+                    ASN1Integer(BigInteger(1, s)),
+                )
+            ).encoded
+        } else {
+            signature
+        }
+        val verifier = Signature.getInstance(algorithm).apply {
+            initVerify(key)
+            update(data)
+        }
+        if (!verifier.verify(javaSignature)) {
+            throw SignatureException()
+        }
+    }
+
+    private fun ByteArray.splitHalves(): Pair<ByteArray, ByteArray> = take(size / 2).toByteArray() to drop(size / 2).toByteArray()
+
+    private fun readPemKeys(data: String): List<PublicKey> {
+        val converter = JcaPEMKeyConverter()
+        return readRawPem(data)
+            .mapNotNull { SubjectPublicKeyInfo.getInstance(it) }
+            .map { converter.getPublicKey(it) }
+            .toList()
+    }
+
+    private fun readRawPem(data: String): Sequence<Any> = sequence {
+        val parser = PEMParser(data.reader())
+        while (true) {
+            val item = parser.readObject()
+                ?: break
+            yield(item)
+        }
     }
 }
---
+++ src/main/kotlin/eu/greenpassapp/greenpassbackend/beans/dgc/impl/TrustList.kt
@@ -0,0 +1,5 @@
+package eu.greenpassapp.greenpassbackend.beans.dgc.impl
+
+class TrustList{
+    var certificates: List<PublicCert> = listOf()
+}
